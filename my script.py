02d8f0235a97c4099ffee23dfa01b3957efb1192
Saimunilepuleley <lepuleleysimon@gmail.com>
2026-06-01 13:12:05 +0300

    Update my script.py

--- my script.py
+++ my script.py
@@ -1,19 +1,15 @@
-python
 import streamlit as st
-st.title("my first stream lit app")
-first_name = st.text_input("enter your first name:")
-surname = st.text_input("enter your surname:")
-st.write("hello", first_name, surname)
-age = st.number_input("enter your age:",min_value=5,max_value=85)
-# code outside the conditional block
-if age >=18:
-    st.write("you can provide your id number because you are adult")
-elif age >= 13:
-    st.write("you are a teenager")
-else:
-    st.write("you are a child")
-    #___code moved outside the conditional block___
-id_number = st.text_input("can i have your id number:",min_characters=6,max_characters=10)
-st.write("thanks we will protect your information, so dont worry:")
-country = st.text_input("which country are you from:")
-st.write("ooh you are from", country, "that's so great")
+st.title("welcome to my first unity app")
+st.write("you are welcomed kijanaa")
+st.write("hello! my name is saimuni na ukipenda zaidi ni simon liam from kenya")
+st.header("tell me about yourself")
+name=st.text_input("what is your name bro and dont put jokes?")
+age=st.number_input("how old are you?",min_value=12,max_value=75)
+country=st.text_input("whre are you from?")
+if st.button("submit"):
+    st.success(f"hello {name} you are {age} years from {country}")
+    if age>=18:
+        st.write("i hope uko kadi")
+    else:
+        st.write("sorry but tulia tukutolee ruto kwa kiti")
+                             
